c970fa0fc90e0a79c9ef8ee28228665cd45907ca
muhammad-hammad <hammy.pk30@gmail.com>
2021-08-16 19:46:30 +0500

    Work in Progress

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const { ApolloServer } = require("apollo-server");
+const fs = require("fs");
+const path = require("path");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const { getUserId } = require("./utils");
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
+
+const resolvers = {
+  Query,
+  Mutation,
+  User,
+  Link,
+};
+
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
+  resolvers,
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      userId: req && req.headers.authorization ? getUserId(req) : null,
+    };
+  },
+});
+
+server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
---
+++ src/script.js
@@ -0,0 +1,28 @@
+// 1
+const { PrismaClient } = require("@prisma/client");
+
+// 2
+const prisma = new PrismaClient();
+
+// 3
+async function main() {
+  const newLink = await prisma.link.create({
+    data: {
+      description: "FullStack GraphQL",
+      url: "www.www.com",
+    },
+  });
+
+  const allLinks = await prisma.link.findMany();
+  console.log(allLinks);
+}
+
+// 4
+main()
+  .catch((e) => {
+    throw e;
+  })
+  // 5
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
